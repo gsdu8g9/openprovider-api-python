957e47f9e0183227cb2e6c537711710f91373493
Roald Nefs <roald@codedholland.nl>
2016-11-04 15:36:28 +0100

    updated Domain object

--- OpenProvider/API/Object/Domain.py
+++ OpenProvider/API/Object/Domain.py
@@ -3,5 +3,9 @@ from .Base import Base
 class Domain(Base):
     @property
     def full_domain(self):
-        if self.domain.name and self.domain.extension:
-            return "{0}.{1}".format(self.domain.name, self.domain.extension)
+        """Get the full domain including the name and extension"""
+        if hasattr(self, 'domain'):
+            if isinstance(self.domain, str):
+                return self.domain
+            if hasattr(self.domain, 'name') and hasattr(self.domain, 'extension'):
+                return "{0}.{1}".format(self.domain.name, self.domain.extension)
