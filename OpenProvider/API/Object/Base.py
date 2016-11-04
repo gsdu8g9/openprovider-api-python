3852357f6d0c5d3ed0e0a83084d1050702bd499d
Roald Nefs <roald@codedholland.nl>
2016-11-04 12:57:17 +0100

    updated OpenProvider

--- OpenProvider/API/Object/Base.py
+++ OpenProvider/API/Object/Base.py
@@ -1,17 +1,31 @@
+import re
+
+def convert_to_snake(string):
+    """Convert camel to snake case"""
+    # Add an underscore before a single uppercase character
+    string = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
+    # Add an underscore before a group of numbers
+    string = re.sub('(.)([0-9]+)', r'\1_\2', string)
+    # Add an underscore before a uppercase character that has a lowercase
+    # character before it
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', string).lower()
+
 class Map(dict):
     def __init__(self, *args, **kwargs):
         super(Map, self).__init__(*args, **kwargs)
         for arg in args:
             if isinstance(arg, dict):
                 for key, value in arg.items():
+                    # Convert camel to snake case
+                    key = convert_to_snake(key)
                     if isinstance(value, dict):
                         self[key] = Map(value)
                     else:
                         self[key] = value
 
         if kwargs:
-            for k, v in kwargs.items():
-                self[k] = v
+            for key, value in kwargs.items():
+                self[key] = value
 
     def __getattr__(self, attr):
         return self.get(attr)
